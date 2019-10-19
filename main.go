4792bdf8b7108dce3b443f4b965d4c648b996da9
Matthew Westphall <westphallm1@protonmail.com>
2019-10-19 15:07:39 -0500

    basic jpeg conversion

--- main.go
+++ main.go
@@ -1,27 +1,31 @@
 package main
-import (
-	"strings"
-)
 
+import "strings"
+
+var TEST_IMAGE = "/home/mwestphall/Pictures/squidward.jpg"
 
 func main() {
 	var sb strings.Builder
-	printBg24(&sb, RGB {0, 0, 0})
-	for i := 0; i < 77; i ++ {
-		g := i*510/76
-		if g > 255 {
-			g = 510 - g
-		}
-		rgb := RGB {
-			byte(255 - (i*255)/76),
-			byte(g),
-			byte(i*255/76),
-		}
-		printFg24(&sb, rgb)
-		sb.WriteRune(rgb.getRune())
-	}
+	printBg24(&sb, RGB{0, 0, 0})
+	// for i := 0; i < 77; i ++ {
+	// 	g := i*510/76
+	// 	if g > 255 {
+	// 		g = 510 - g
+	// 	}
+	// 	rgb := RGB {
+	// 		byte(255 - (i*255)/76),
+	// 		byte(g),
+	// 		byte(i*255/76),
+	// 	}
+	// 	printFg24(&sb, rgb)
+	// 	sb.WriteRune(rgb.getRune())
+	// }
+	// printTerm(&sb)
+	// str := sb.String()
+	// println(str)
+	// println(len(str))
+	img := downscaleImage(readImage(TEST_IMAGE), 40, 40)
+	img.printTo(&sb)
 	printTerm(&sb)
-	str := sb.String()
-	println(str)
-	println(len(str))
+	println(sb.String())
 }
--- pretty_print.go
+++ pretty_print.go
@@ -1,65 +1,111 @@
 package main
 
 import (
-	"strings"
-	"strconv"
 	"image"
-	"os"
+	"image/jpeg"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
-type ImageChunk struct {
-}
-
 type ImageConvert struct {
-	image * image.Image
-	bounds  image.Rectangle
-	width, height int
-	downscaled [] RGB
+	image          image.Image
+	bounds         image.Rectangle
+	width, height  int
+	downscaled     []RGB
 	xScale, yScale int
 }
-func (convert * ImageConvert) samplePixels(x, y int) {
-	startX := convert.bounds.Min.X + x * convert.xScale
-	endX := startX + convert.xScale
-	startY := convert.bounds.Min.Y +  y * convert.yScale
-	endY := startY + convert.yScale
-} 
 
-func downscaleImage(filePath string, height int, width int) []RGB{
+func (convert *ImageConvert) set(x, y int, value RGB) {
+	convert.downscaled[x*convert.width+y] = value
+}
+
+func (convert *ImageConvert) samplePixels(x, y int) {
+	startX := convert.bounds.Min.X + x*convert.xScale
+	endX := startX + convert.xScale
+	startY := convert.bounds.Min.Y + y*convert.yScale
+	endY := startY + convert.yScale
+	nPixels := uint32((endX - startX) * (endY - startY)) * 0x101
+	if convert.bounds.Max.X < endX {
+		endX = convert.bounds.Max.X
+	}
+	if convert.bounds.Max.Y < endY {
+		endY = convert.bounds.Max.Y
+	}
+	var r, g, b uint32
+	for i := startX; i < endX; i++ {
+		for j := startY; j < endY; j++ {
+			r1, g1, b1, _ := convert.image.At(i, j).RGBA()
+			r += r1
+			g += g1
+			b += b1
+		}
+	}
+	r /= nPixels
+	g /= nPixels
+	b /= nPixels
+	convert.set(x, y, RGB{byte(r), byte(g), byte(b)})
+}
+
+func downscaleImage(image image.Image, height int, width int) ImageConvert {
+	bounds := image.Bounds()
+	convert := ImageConvert{
+		image:      image,
+		bounds:     bounds,
+		width:      width,
+		height:     height,
+		downscaled: make([]RGB, width*height),
+	}
+
+	convert.xScale = (bounds.Max.X - bounds.Min.X) / width
+	convert.yScale = (bounds.Max.Y - bounds.Min.Y) / height
+
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			convert.samplePixels(i, j)
+		}
+	}
+	return convert
+}
+
+func (convert *ImageConvert) printTo(sb *strings.Builder) {
+	for j := 0; j < convert.height; j++ {
+		for i := 0; i < convert.width; i++ {
+			rgb := convert.downscaled[convert.width *i + j]
+			printFg24(sb, rgb)
+			sb.WriteRune(rgb.getRune())
+		}
+		sb.WriteRune('\n')
+	}
+	for i := 0; i < convert.width*convert.height; i++ {
+	}
+}
+
+func (convert *ImageConvert) toString() string {
+	var sb strings.Builder
+	convert.printTo(&sb)
+	return sb.String()
+}
+
+func readImage(filePath string) image.Image {
 	reader, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer reader.Close()
-	image, _, err := image.Decode(reader)
+	image, err := jpeg.Decode(reader)
 	if err != nil {
 		log.Fatal(err)
 	}
-	bounds := image.Bounds()
-	convert := ImageConvert {
-		image: &image, 
-		bounds: bounds,
-		width: width, 
-		height: height, 
-		downscaled: make([]RGB, width * height),
-	}
-
-	convert.xScale = (bounds.Max.X - bounds.Min.X) / width
-	convert.yScale = (bounds.Max.Y - bounds.Min.Y) / width
-	
-	for i := 0; i < width; i ++ {
-		for j := 0; j < height; j++ {
-			convert.samplePixels(i, j)
-		}
-	}
-	return convert.downscaled
+	return image
 }
 
 type RGB struct {
 	r, g, b byte
 }
 
-func (rgb * RGB) printEscape(sb * strings.Builder) {
+func (rgb *RGB) printEscape(sb *strings.Builder) {
 	sb.WriteString(strconv.FormatInt(int64(rgb.r), 10))
 	sb.WriteRune(';')
 	sb.WriteString(strconv.FormatInt(int64(rgb.g), 10))
@@ -67,7 +113,7 @@ func (rgb * RGB) printEscape(sb * strings.Builder) {
 	sb.WriteString(strconv.FormatInt(int64(rgb.b), 10))
 }
 
-func (rgb * RGB) getRune() rune {
+func (rgb *RGB) getRune() rune {
 	switch brightness := int(rgb.r) + int(rgb.g) + int(rgb.b); {
 	case brightness < 100:
 		return '*'
@@ -91,7 +137,7 @@ func (rgb * RGB) getRune() rune {
 /*
  *Print the escape sequence for a 24-bit color foreground
  */
-func printFg24(sb * strings.Builder, rgb RGB) {
+func printFg24(sb *strings.Builder, rgb RGB) {
 	sb.WriteString("\033[38;2;")
 	rgb.printEscape(sb)
 	sb.WriteRune('m')
@@ -100,7 +146,7 @@ func printFg24(sb * strings.Builder, rgb RGB) {
 /*
  * Print the escape sequence for a 24-bit color background
  */
-func printBg24(sb * strings.Builder, rgb RGB) {
+func printBg24(sb *strings.Builder, rgb RGB) {
 	sb.WriteString("\033[48;2;")
 	rgb.printEscape(sb)
 	sb.WriteRune('m')
@@ -109,8 +155,6 @@ func printBg24(sb * strings.Builder, rgb RGB) {
 /*
  * Print the escape sequence for clearing the fore/background
  */
-func printTerm(sb * strings.Builder) {
+func printTerm(sb *strings.Builder) {
 	sb.WriteString("\033[0m")
 }
-
-
